88322615fa7143db742e6d929b7677c4938d1be4
Marie Thompson <mariethompson35@gmail.com>
2021-03-08 08:59:22 -0700

    initial src files for parcel-builder

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+// contains the code needed to create a small, working React app
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// Import statement to indicate that you need to bundle `./index.scss`
+import './index.scss';
+
+// Main component (will eventually use all the others)
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-Flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
+}
+
+// Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render your app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
