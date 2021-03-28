c94c7b4ff4828391e17ceb69a41f812fd389199f
Marie Thompson <mariethompson35@gmail.com>
2021-03-28 14:22:45 -0700

    Add prop-types & add'tl properties in display
    Added: Year and Director Bio

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MainView } from '../main-view/main-view';
+import PropTypes from 'prop-types';
 
 import './movie-view.scss';
 
@@ -23,6 +24,10 @@ export class MovieView extends React.Component {
           <span className='label'>Title: </span>
           <span className='value'>{movie.Title}</span>
         </div>
+        <div className='movie-year'>
+          <span className='label'>Release Year: </span>
+          <span className='value'>{movie.Year}</span>
+        </div>
         <div className='movie-description'>
           <span className='label'>Description: </span>
           <span className='value'>{movie.Description}</span>
@@ -36,6 +41,10 @@ export class MovieView extends React.Component {
           <span className='label'>Director: </span>
           <span className='value'>{movie.Director.Name}</span>
         </div>
+        <div className='movie-director-bio'>
+          <span className='label'>Director Bio: </span>
+          <span className='value'>{movie.Director.Bio}</span>
+        </div>
         <div>
         <button className='home-button' onClick={() => onClick()}>Home</button>
         </div>
@@ -44,3 +53,20 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Year: PropTypes.number.isRequired, //note - this might need to be considered a string
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired
+    })
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
