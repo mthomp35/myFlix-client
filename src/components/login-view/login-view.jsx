cc4bca09494059711b1a4b7834717334a74efcb7
Marie Thompson <mariethompson35@gmail.com>
2021-03-29 17:20:11 -0700

    Add bootstrap format to form and button

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Form, Button } from 'react-bootstrap';
 
 import './login-view.scss';
 
@@ -14,17 +15,33 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type='text' value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type='text' value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type='submit' onClick={handleSubmit}>Submit</button>
-    </form>
+    <Form>
+      <Form.Group controlID='formUsername'>
+        <Form.Label>Username:</Form.Label>
+        <Form.Control 
+          type='text' 
+          value={username}
+          onChange={e => setUsername(e.target.value)} 
+          placeholder='Enter username'
+        />
+      </Form.Group>
+      
+      <Form.Group controlID='formPassword'>
+        <Form.Label>Password:</Form.Label>
+        <Form.Control 
+          type='text'
+          value={password}
+          aria-describedby='passwordHelpBlock'
+          onChange={e => setPassword(e.target.value)}
+          placeholder='Enter password'
+        />
+        <Form.Text id='passwordHelpBlock' muted>
+          Password must contain: At least 10 characters, a combination of uppercase and lowercase letters (A-z), numbers (0-9), and special characters (e.g. ! @ # ? ]
+        </Form.Text>
+      </Form.Group>
+      
+      <Button type='submit' variant='secondary' onClick={handleSubmit}>Submit</Button>
+    </Form>
   );
 }
 
