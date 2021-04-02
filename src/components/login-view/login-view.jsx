729a8d1b07e8bc15f864c3244cd8bdd767cd6439
Marie Thompson <mariethompson35@gmail.com>
2021-04-02 16:13:02 -0700

    Add Form Rows & Columns

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col } from 'react-bootstrap';
 
 import './login-view.scss';
 
@@ -16,6 +16,12 @@ export function LoginView(props) {
 
   return (
     <Form className='login'>
+      <Form.Row>
+        <h1>Welcome to Movie Mania!</h1>
+      </Form.Row>
+      <Form.Row>
+        <p>Please log in to to continue.</p>
+      </Form.Row>
       <Form.Group controlId='formUsername'>
         <Form.Label>Username:</Form.Label>
         <Form.Control 
@@ -35,12 +41,18 @@ export function LoginView(props) {
           onChange={e => setPassword(e.target.value)}
           placeholder='Enter Password'
         />
-        <Form.Text id='passwordHelpBlock' muted>
-          Password must contain: At least 10 characters, a combination of uppercase and lowercase letters (A-z), numbers (0-9), and special characters (e.g. ! @ # ? ]
+        <Form.Text id='passwordHelpBlock'>
+          Password must contain: At least 10 characters, a combination of uppercase and lowercase letters (A-z), numbers (0-9), and special characters (e.g. ! @ # ? ] ).
         </Form.Text>
       </Form.Group>
-      
-      <Button type='submit' variant='secondary' onClick={handleSubmit}>Submit</Button>
+      <Form.Row>
+        <Col md={6}>
+          <Button type='submit' variant='secondary' onClick={handleSubmit}>Submit</Button>
+        </Col>
+        <Col md={6}>
+          <Button type='button' variance='link'>New to Movie Mania? Click here to register (Reminder: link this to log in registration page)</Button>
+        </Col>
+      </Form.Row>
     </Form>
   );
 }
