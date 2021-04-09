bfd94456599b86d15c6c6308465e7a9f8451c615
Marie Thompson <mariethompson35@gmail.com>
2021-04-08 17:01:29 -0700

    Update onLoggedIn to pull authentication data and store locally

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -48,10 +48,15 @@ export class MainView extends React.Component {
   }
 
   // When a user successfully logs in, this function updates the `user` property in state to that particular user
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user
+      user: authData.user.Username
     });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
   }
 
   // 
