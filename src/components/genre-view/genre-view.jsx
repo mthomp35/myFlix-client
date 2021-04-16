f19c95d208ab6f9bf7177cb3bc2eb67a08e18555
Marie Thompson <mariethompson35@gmail.com>
2021-04-16 07:47:22 -0700

    Add basic layout for genre view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -3,5 +3,25 @@ import PropTypes from 'prop-types';
 import { Nav, Form, Button } from 'react-bootstrap';
 
 export class GenreView extends React.Component {
-  
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    const { genre } = this.props;
+
+    return(
+      <div className='genre-view'>
+        <Nav>
+
+        </Nav>
+        <div className='gv'>
+          <div className='gv-name'>{genre.Name}</div>
+          <div className='description'>{genre.Description}</div>
+        </div>
+      </div>
+    )
+  }
 }
