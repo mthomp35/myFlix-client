ac9ffd4237292f4948a86277da435c82cdadc2c3
Marie Thompson <mariethompson35@gmail.com>
2021-04-16 07:47:56 -0700

    Add loading message prop and movies filter for director view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -19,7 +19,8 @@ export class MainView extends React.Component {
 
     this.state = {
       movies: [],
-      user: null
+      user: null,
+      message: 'Loading'
     };
   }
 
@@ -57,7 +58,7 @@ export class MainView extends React.Component {
 
   getMovies(token) {
     axios.get('https://best-flix-10922.herokuapp.com/movies', {
-      headers: { Authorization: 'Bearer ${token}'}
+      headers: { Authorization: `Bearer ${token}`}
     })
     .then(response => {
       // Assign the result to the state
@@ -67,17 +68,20 @@ export class MainView extends React.Component {
     })
     .catch(function (error) {
       console.log(error);
+      this.setState({
+        message: 'Something went wrong'
+      });
     });
   }
 
   render() {
     //If the state isn't initialized, this will throw on runtime
     //before the data is initially loaded
-    const { movies, user } = this.state;
+    const { movies, user, message } = this.state;
 
     //before the movies have been loaded
     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView*/
-    if (!movies) return <div className='main-view'/>;
+    if (!movies.length) return <div className='main-view'>{message}</div>;
     return (
       <Router>
         <div className='main-view'>
@@ -87,14 +91,14 @@ export class MainView extends React.Component {
             }
           }/>
           <Route path='/register' render={() => <RegistrationView/>} />
-          <Route path='/movies/:movieId' render={({match}) => <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>}/>
+          <Route path='/movies/:movieId' render={({match, history}) => <MovieView history={history} movie={movies.find(m => m._id === match.params.movieId)}/>}/>
           <Route path='/genre/:name' render={({match}) => {
-            if (!movies) return <div className='main-view'/>;
+            if (!movies.length) return <div className='main-view'>{message}</div>;
             return <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre}/>}
           } />
           <Route path='/directors/:name' render={({match}) => {
-            if (!movies) return <div className='main-view'/>;
-            return <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director}/>}
+            if (!movies.length) return <div className='main-view'>{message}</div>;
+            return <DirectorView movies={movies.filter(m => m.Director.Name === match.params.name)} director={movies.find(m => m.Director.Name === match.params.name).Director}/>}
           } />
           <Route path='/users/:Username' render={() => <ProfileView/>} />
         </div>
