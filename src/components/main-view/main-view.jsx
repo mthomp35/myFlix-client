ddb2af5861acefb1fca9ba6645f49c114484c548
Marie Thompson <mariethompson35@gmail.com>
2021-04-18 12:43:30 -0700

    Import ProfileView jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,6 +9,7 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
+import {ProfileView } from '../profile-view/profile-view';
 
 import './main-view.scss';
 
