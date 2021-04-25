b6513c5865b3463e42611daf91a884cfd07de824
Marie Thompson <mariethompson35@gmail.com>
2021-04-25 08:59:37 -0700

    Add Form layout

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -84,6 +84,10 @@ export class ProfileView extends React.Component {
     });
   }
 
+  editProfile() {
+
+  }
+
   render() {
     const { firstName, lastName, email, dob, username, password, favoriteMovies, history } = this.state;
     const { movies } = this.props;
@@ -92,25 +96,92 @@ export class ProfileView extends React.Component {
 
     return(
       <div>
-        <div>
-          <span>First Name: {firstName}</span>
-        </div>
-        <div>
-          <span>Username: {username}</span>
-        </div>
-        <div>
-          Change your password
-          <div>
-            Current password:
-          </div>
-          <div>
-            New password:
-          </div>
-          <div>
-            Re-enter new password:
-          </div>
-        </div>
-        <div> Favorite Movies:
+        <Form className='update-profile'>
+          <p>Edit ${username} profile information.</p>
+          <Form.Group controlId='formFirstName'>
+            <Form.Label>First Name:</Form.Label>
+            <Form.Control
+              type='text'
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+              placeholder={this.props.firstName}
+            />
+          </Form.Group>
+      
+          <Form.Group controlId='formLastName'>
+            <Form.Label>Last Name:</Form.Label>
+            <Form.Control
+              type='text'
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+              placeholder='Enter Last Name'
+              //srOnly='Enter Last Name'
+            />
+          </Form.Group>
+          
+          <Form.Group controlId='formEmail'>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type='email'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder='Enter Email'
+              //srOnly='Enter email address'
+            />
+          </Form.Group>
+          
+          <Form.Group controlId='formBirthday'>
+            <Form.Label>Birthday:</Form.Label>
+            <Form.Control
+              type='text'
+              value={birthday}
+              onChange={e => setBirthday(e.target.value)}
+              placeholder='Enter Date of Birth'
+              //srOnly='Enter date of birth (month/day/year)'
+            />
+          </Form.Group>
+
+          <Form.Group controlId='formUsername'>
+            <Form.Label>Username:</Form.Label>
+            <Form.Control
+              type='text'
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              placeholder='Enter Username'
+              //srOnly='Enter username'
+            />
+          </Form.Group>
+
+          <Form.Group controlId='formPassword'>
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type='password'
+              value={password}
+              aria-describedby='passwordHelpBlock'
+              onChange={e => setPassword(e.target.value)}
+              placeholder='Enter Password'
+              //srOnly='Enter password'
+            />
+            <Form.Text id='passwordHelpBlock'>
+              Password must contain: At least 10 characters, a combination of uppercase and lowercase letters (A-z), 
+              numbers (0-9), and special characters (e.g. ! @ # ? ] ).
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId='formConfirmPassword'>
+            <Form.Label>Confirm Password:</Form.Label>
+            <Form.Control
+              type='password'
+              value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)}
+              placeholder='Confirm Password'
+              //srOnly='Re-enter password to confirm'
+            />
+          </Form.Group>
+
+          <Button type='submit' variant='secondary' onClick={handleRegister}>Submit</Button>
+        </Form>
+        <div>Favorite Movies:
           {favMovies.map((fav, index) => {
             return(
             <div key={index}>
