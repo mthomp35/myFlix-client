83849203343f6b3ce09a7cb061191ff9aff5640c
Marie Thompson <mariethompson35@gmail.com>
2021-05-10 08:08:36 -0700

    remove 'user' prop

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -8,7 +8,7 @@ import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, user } = this.props;
+    const { movie } = this.props;
 
     return (
       <div>
