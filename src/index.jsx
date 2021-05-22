cb891a6f5c7f742118131eb813a4909554642139
Marie Thompson <mariethompson35@gmail.com>
2021-05-22 14:31:29 -0700

    Add the store for redux

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Container } from 'react-bootstrap';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
 
 import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp);
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
