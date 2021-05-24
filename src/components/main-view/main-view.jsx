044f5df387afa456b30a512bea0a126f62c7beb6
Marie Thompson <mariethompson35@gmail.com>
2021-05-24 09:30:37 -0700

    Add setUser to mapStateToProps and export default

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -153,7 +153,7 @@ class MainView extends React.Component {
 }
 
 let mapStateToProps = state => {
-  return { movies: state.movies }
+  return { movies: state.movies, user: state.user }
 }
 
-export default connect(mapStateToProps, { setMovies })(MainView);
+export default connect(mapStateToProps, { setMovies, setUser })(MainView);
