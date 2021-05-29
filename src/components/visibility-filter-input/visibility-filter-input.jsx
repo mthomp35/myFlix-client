d64c310c4bf0f33fb0a61ea14b14ee935a0a270a
Marie Thompson <mariethompson35@gmail.com>
2021-05-29 14:52:23 -0700

    Add additional filtering context

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -8,7 +8,7 @@ function VisibilityFilterInput(props) {
   return <Form.Control
     onChange={e => props.setFilter(e.target.value)}
     value={props.visibilityFilter}
-    placeholder='Filter'
+    placeholder='Filter movies by title'
   />;
 }
 
